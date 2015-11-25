ba197f345c67664e53121f648a6eca8b7ab29716
Magnus Fjell <magnus.fjell@gmail.com>
2015-11-25 22:47:16 +0100

    Change filename, get comics from more sources

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
 var request = require("request");
 var fs = require("fs");
 var sessionid ='<SESSION ID FROM COMICS.IO>';
-var url = 'https://comics.io/lunchdb/2015/11/25/';
+var urls = ['/lunchdb/2015/11/18/', '/lunch/2013/6/28/', '/lunche24/2015/11/24/', '/lunchtu/2015/11/24/'];
 var baseUrl = 'https://comics.io';
 
 
-var options = {
-  url: url,
-  headers : {
-    "Cookie": "sessionid="+sessionid
-  }
-};
-
-var imgUrlregex = /<img src="(.*?)"/
-var imgNameRegex = /.*\/(.*)$/
-var previousRegex = /<a href="([^"]*?)" class="btn btn-primary " id="prev"/
-
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var imgUrl = imgUrlregex.exec(body);
-    var imgName = imgNameRegex.exec(imgUrl[1]);
-    var prevImg = previousRegex.exec(body);
-    request(imgUrl[1]).pipe(fs.createWriteStream("stripes/" + imgName[1]));
-    if(!prevImg) { return; }
-    url = baseUrl + prevImg[1];
-    options.url = url;
-    request(options, callback);
-  }
+for (var i = 0; i < urls.length; i++) {
+  crawl(urls[i]);
 }
 
-request(options, callback);
+function crawl(url) {
+  var options = {
+    url: baseUrl + url,
+    headers : {
+      "Cookie": "sessionid="+sessionid
+    }
+  };
+
+  var imgUrlregex = /<img src="(.*?)".*?alt="([^"]*?)"/;
+  var previousRegex = /<a href="([^"]*?)" class="[^"]*?" id="prev"/;
+  var latestRegex = /<a href="([^"]*?)" class="[^"]*?" id="last"/;
+  var extensionRegex = /.+\.([^?]+)(\?|$)/;
+
+  function findLatest(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var lastUrl = latestRegex.exec(body);
+      if(!lastUrl){
+        //already on latest
+      }
+      else {
+        options.url = baseUrl + lastUrl[1];
+      }
+      request(options, crawl);
+    }
+  }
+
+  function crawl(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var imgUrl = imgUrlregex.exec(body);
+      var imgDate = imgUrl[2].split(" ")[1].replace("/", "-") +"."+ extensionRegex.exec(imgUrl[1])[1];
+      var prevImg = previousRegex.exec(body);
+      var path = "comics/" + imgDate;
+
+      try {
+        fs.accessSync(path);
+        console.log("Already downloaded this comic, and subsequent ones if crawler has been run correctly.")
+        return;
+      } catch(ex) {
+
+      }
+      request(imgUrl[1]).pipe(fs.createWriteStream(path));
+      if(!prevImg) { return; }
+      url = baseUrl + prevImg[1];
+      options.url = url;
+      request(options, crawl);
+    }
+  }
+
+  request(options, findLatest);
+}
